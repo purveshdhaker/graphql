870aed45893c692688613e31e2508bdbb46e4cb5
purvesh dhaker <pdhaker@griddynamics.com>
2024-12-19 12:35:32 +0530

    graphql

---
+++ src/main/java/com/graphql/tutorial/bookdetails/BookController.java
@@ -0,0 +1,21 @@
+package com.graphql.tutorial.bookdetails;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+class BookController {
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+
+}
---
+++ src/test/java/com/graphql/tutorial/bookdetails/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.tutorial.bookdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
